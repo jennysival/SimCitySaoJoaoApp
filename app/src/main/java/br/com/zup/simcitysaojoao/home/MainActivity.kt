895ace5f106c30be42d9ec96c0abaa64fedb9f22
jennysival <jeniffersival@gmail.com>
2022-06-10 16:20:12 -0300

    Limpar fragmentos

--- app/src/main/java/br/com/zup/simcitysaojoao/home/MainActivity.kt
+++ app/src/main/java/br/com/zup/simcitysaojoao/home/MainActivity.kt
@@ -1,12 +1,23 @@
 package br.com.zup.simcitysaojoao.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.zup.simcitysaojoao.R
+import br.com.zup.simcitysaojoao.databinding.ActivityMainBinding
+import br.com.zup.simcitysaojoao.produtos.ProdutosActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnIrParaProdutos.setOnClickListener {
+            startActivity(Intent(this, ProdutosActivity::class.java))
+        }
     }
 }
