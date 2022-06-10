7495e998ae6df580b83bbc7a4bcaea7e98dcf5d7
jennysival <jeniffersival@gmail.com>
2022-06-10 14:12:10 -0300

    Adicionar botão de voltar

--- app/src/main/java/br/com/zup/simcitysaojoao/produtos/ProdutosActivity.kt
+++ app/src/main/java/br/com/zup/simcitysaojoao/produtos/ProdutosActivity.kt
@@ -2,11 +2,23 @@ package br.com.zup.simcitysaojoao.produtos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import br.com.zup.simcitysaojoao.R
 
 class ProdutosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_produtos)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setTitle(R.string.titulo_activity_produtos)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            this.finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
