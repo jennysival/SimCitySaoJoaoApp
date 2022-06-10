ccb024a34d22065ba6b17e964e5d4e60f7db8339
jennysival <jeniffersival@gmail.com>
2022-06-10 13:58:18 -0300

    Criar activities e objeto

---
+++ app/src/main/java/br/com/zup/simcitysaojoao/produtos/cadastro/CadastroProdutosActivity.kt
@@ -0,0 +1,12 @@
+package br.com.zup.simcitysaojoao.produtos.cadastro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.zup.simcitysaojoao.R
+
+class CadastroProdutosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_produtos)
+    }
+}
---
+++ app/src/main/java/br/com/zup/simcitysaojoao/produtos/valorTotal/ValorTotalActivity.kt
@@ -0,0 +1,12 @@
+package br.com.zup.simcitysaojoao.produtos.valorTotal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.zup.simcitysaojoao.R
+
+class ValorTotalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_valor_total)
+    }
+}
