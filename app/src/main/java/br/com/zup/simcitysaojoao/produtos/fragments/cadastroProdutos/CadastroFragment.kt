34a1df8bdf5af67500fea30d5c64f80104870a08
jennysival <jeniffersival@gmail.com>
2022-06-10 16:56:25 -0300

    Adicionar atributos ao objeto

--- app/src/main/java/br/com/zup/simcitysaojoao/produtos/fragments/cadastroProdutos/CadastroFragment.kt
+++ app/src/main/java/br/com/zup/simcitysaojoao/produtos/fragments/cadastroProdutos/CadastroFragment.kt
@@ -6,15 +6,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import br.com.zup.simcitysaojoao.R
+import br.com.zup.simcitysaojoao.databinding.FragmentCadastroBinding
 
 class CadastroFragment : Fragment() {
+    private lateinit var binding: FragmentCadastroBinding
+    private lateinit var nomeProduto: String
+    private lateinit var qtdProduto: String
+    private lateinit var valorProduto: String
+    private lateinit var receitaProduto: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cadastro, container, false)
+    ): View {
+        binding = FragmentCadastroBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnCadastrarNovo.setOnClickListener {
+
+        }
+
+        binding.btnVerProdutos.setOnClickListener {
+
+        }
+
+        binding.btnValorTotal.setOnClickListener {
+
+        }
     }
 
 }
--- app/src/main/java/br/com/zup/simcitysaojoao/produtos/model/Produto.kt
+++ app/src/main/java/br/com/zup/simcitysaojoao/produtos/model/Produto.kt
@@ -1,4 +1,25 @@
 package br.com.zup.simcitysaojoao.produtos.model
 
-class Produto {
+import kotlinx.parcelize.Parcelize
+import android.os.Parcelable
+
+@Parcelize
+class Produto(private var nome: String,
+              private var quantidade: Double,
+              private var valorUn: Double,
+              private var receita: String
+): Parcelable {
+    private var valorTotal = 0.0
+
+    fun getNome() = this.nome
+    fun getQtd() = this.quantidade
+    fun getValorUn() = this.valorUn
+    fun getReceita() = this.receita
+    fun getValorTotal() = calcularTotal()
+
+    private fun calcularTotal(): Double{
+        valorTotal = quantidade*valorUn
+
+        return valorTotal
+    }
 }
